fa37c6c1536ec39b8e6f7259ff0425f5bae80e92
gzeon <95478735+gzeoneth@users.noreply.github.com>
2022-10-18 05:33:20 +0800

    chore: RPC_URL envvar

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,7 @@
 import ERC20Abi from "./abi/ERC20.json";
 import { ethers } from "ethers";
 
-const RPC_URL =
-  "https://solemn-omniscient-borough.arbitrum-mainnet.quiknode.pro/69c7aa690b0d1164b85b62e06251b25e2fb85171/";
+const RPC_URL = process.env.RPC_URL;
 const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
 
 function sleep(ms) {
